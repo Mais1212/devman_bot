d1a1416b27735e120fde004508d9a414af54a78f
Mais1212 <64730510+Mais1212@users.noreply.github.com>
2022-07-12 10:05:18 +0300

    Main update

--- main.py
+++ main.py
@@ -24,12 +24,26 @@ def start_long_pooling(devman_token, telegram_token, telegram_chat_id):
             )
             response.raise_for_status()
 
-            if response.json()['status'] == 'timeout':
+            response = response.json()
+            if response['status'] == 'timeout':
                 params = {
-                    'timestamp': response.json()['timestamp_to_request']
+                    'timestamp': response['timestamp_to_request']
+                }
+                continue
+            elif response['status'] == 'found':
+                params = {
+                    'timestamp': response['last_attempt_timestamp']
                 }
 
-            send_message(bot, response.json(), telegram_chat_id)
+            lesson_title = response["new_attempts"][0]["lesson_title"]
+            lesson_url = response["new_attempts"][0]["lesson_url"]
+            message = f'Работа [{lesson_title}]({lesson_url}) готова'
+
+            bot.send_message(
+                text=message,
+                chat_id=telegram_chat_id,
+                parse_mode='MarkdownV2'
+            )
 
         except requests.exceptions.ReadTimeout:
             pass
@@ -38,18 +52,6 @@ def start_long_pooling(devman_token, telegram_token, telegram_chat_id):
             pass
         except requests.exceptions.HTTPError as exception:
             print(exception)
-            exit()
-
-
-def send_message(bot, response_json, telegram_chat_id):
-    lesson_title = response_json["new_attempts"][0]["lesson_title"]
-    lesson_url = response_json["new_attempts"][0]["lesson_url"]
-    message = f'Работа [{lesson_title}]({lesson_url}) готова'
-    bot.send_message(
-        text=message,
-        chat_id=telegram_chat_id,
-        parse_mode='MarkdownV2'
-    )
 
 
 if __name__ == '__main__':
